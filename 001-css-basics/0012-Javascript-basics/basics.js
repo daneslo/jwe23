bfcc29976fab7bf57ea20b6df56815012cc5232f
daneslo <daniela.escobar.lpz@gmail.com>
2023-12-12 19:35:49 +0100

    javascript

---
+++ 001-css-basics/0012-Javascript-basics/basics.js
@@ -0,0 +1,24 @@
+console.log("hier wird debugged!");
+
+
+let myName = "Dani"; //string: Zeichchenkette  string
+let myAge = 33; // Zahl (Ganzzahl)        int oder integer
+let myWeight = 70.2;   //Zahl (DecimaZahl)   Float
+let organicFood = false; //Boolsche Werte (falsch/wahr)  bool
+
+let myList = ["Brot", "Milch", "Ketchup"]; //Liste/Array   array
+
+let dynamischesHTML = '<p style="color:red;">Roter Text</p>';
+
+let dynamischesHTML2= '<p style="color:green;">Grüner Text></p>';
+
+document.body.innerHTML = dynamischesHTML;
+
+let showMyListInConsole = function(){
+    console.log(myList);
+};
+
+
+
+
+
